f4a9ada7cb192d90475a5d857b3e26fcc76da495
Andrew Fawcett <141191160+Foocett@users.noreply.github.com>
2024-04-01 15:47:52 -0500

    Added annotations to client.js

--- Public/client.js
+++ Public/client.js
@@ -1,104 +1,101 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Prompt for username before loading page content
-    console.log("connected");
-    let username = prompt("Please enter your username:");
-    while (username === null || username.trim() === "") {
-        alert("You must enter a username to continue.");
+    let username = prompt("Please enter your username:"); //Prompt for username before loading page content
+    while (username === null || username.trim() === "") { //check valid username
+        alert("You must enter a username to continue."); //if input is invalid, prompt again
         username = prompt("Please enter your username:");
     }
 
     const socket = io(); // Connect to the server
-    let self;
-    const handCards = document.querySelectorAll(".white-card");
-    let handElementsText = []
-    let handElementsPack = []
-    let selectedIndex;
-    let selectedText;
-    let selectedPack;
-    let submittedText;
-    let submittedPack;
-    let hasCardBeenSelected = false;
+    let self; //will be given value after client is initialized with server
+    const handCards = document.querySelectorAll(".white-card"); //all cards in hand
+    let handElementsText = [] //white card text objects
+    let handElementsPack = [] //white card pack objects
+    let selectedIndex; //index of selected card
+    let selectedText; //text of selected card
+    let selectedPack; //pack of selected card
+    let submittedText; //text of submitted card
+    let submittedPack; //pack of submitted card
+    let hasCardBeenSelected = false; //used to prevent submission before selection
+    let hasCardBeenSubmitted = false;
+
+    //get HTML objects from document
     const startGameButton = document.getElementById("start-turn-button");
     const blackText = document.getElementById("black-card-text");
     const blackPack = document.getElementById("black-card-pack");
     const submitButton = document.getElementById("submit-button");
     const publicContainer = document.getElementById("cards-public-container")
-    let playerContainer = document.getElementById("score-display-container")
-    submitButton.disabled = true;
-    function debug(message){
-        socket.emit("debug", message);
+    const playerContainer = document.getElementById("score-display-container")
+    submitButton.disabled = true; //disable submit button by default
+
+    for(let i = 1; i<=7; i++){ //get HTML objects for each card
+        handElementsText.push(document.getElementById("white-card-"+i+"-text")) //text object
+        handElementsPack.push(document.getElementById("white-card-"+i+"-pack")) //pack object
     }
 
-    for(let i = 1; i<=7; i++){
-        handElementsText.push(document.getElementById("white-card-"+i+"-text"))
-        handElementsPack.push(document.getElementById("white-card-"+i+"-pack"))
-    }
-
-    socket.emit('requestPlayerData', username, (response) => {
-        self = response.rawPlayerInfo
-        populateCardsFromHand(self);
-        if(self.admin){
+    socket.emit('requestPlayerData', username, (response) => { //send username to server and get self player object back
+        self = response.rawPlayerInfo //set self equal to returned player object
+        populateCardsFromHand(self); //convert self.hand into HTML elements
+        if(self.admin){ //if admin, show start game button
             startGameButton.style.display = "block";
             startGameButton.onclick = () => {
-                socket.emit("begin-game");
-                startGameButton.style.display = "none";
+                socket.emit("begin-game"); //send begin game signal to server
+                startGameButton.style.display = "none"; //hide after click
             }
         } else {
-            startGameButton.style.display = "none";
+            startGameButton.style.display = "none"; //hide button
         }
     });
 
-    socket.on("updatePlayerList", (playerInfo) => {
+    socket.on("updatePlayerList", (playerInfo) => { //updates list of connected players
         updateConnectedPlayers(playerInfo)
     })
 
 
-    socket.on("start-turn", (gameData) => {
-        hasCardBeenSelected = false;
-        handCards.forEach(card => {
+    socket.on("start-turn", (gameData) => { //on start turn command receive
+        hasCardBeenSelected = false; //reset selected cards
+        hasCardBeenSubmitted = false;
+        handCards.forEach(card => { //remove selected class from all HTML objects
             card.classList.remove("selected-card")
         })
-        socket.emit('update-self', username, (response) => {
+        socket.emit('update-self', username, (response) => { //requests updated personal info
             self = response.rawPlayerInfo
-            setCardsClickable(!self.czar);
+            setCardsClickable(!self.czar); //if player is czar, make hand cards unclickable
         });
 
-        submitButton.disabled = self.czar;
-        blackText.textContent = gameData.currentBlackCard.text;
-        blackText.textContent = gameData.currentBlackCard.text;
-        blackPack.textContent =  getProperName(gameData.currentBlackCard.pack);
-        updateConnectedPlayers(gameData.players);
-
+        submitButton.disabled = self.czar; //if player is czar, disable submit button
+        blackText.textContent = gameData.currentBlackCard.text; //set black card text
+        blackPack.textContent =  getProperName(gameData.currentBlackCard.pack); //get full pack name
+        updateConnectedPlayers(gameData.players); //updates connected players
     });
 
 
-    function populateCardsFromHand(self) {
-        for(let i = 0; i<7;i++){
-            handElementsText[i].textContent = self.hand[i].text;
-            handElementsPack[i].textContent = getProperName(self.hand[i].pack);
+    function populateCardsFromHand(self) { //update HTML cards with info in hand
+        for(let i = 0; i<7;i++){ //for each of seven cards
+            handElementsText[i].textContent = self.hand[i].text; //set text to card.text
+            handElementsPack[i].textContent = getProperName(self.hand[i].pack); //set pack to card.pack's full name
         }
     }
 
-    for(let i=0;i<7;i++){
+    for(let i=0;i<7;i++){ //add click listeners to each card
         handCards[i].addEventListener('click', function() {
-            if(this.classList.contains("clickable")){
-                selectedIndex = i;
-                selectedText = self.hand[i].text;
-                selectedPack = self.hand[i].pack;
-                handCards.forEach(element => {
-                    element.classList.remove("selected-card");
-                    element.style.backgroundColor = "white"; // Reset background color
-                    element.style.color = "black"; // Reset text color
+            if(this.classList.contains("clickable")){ //if the card is currently clickable...
+                selectedIndex = i; //set selected card index to position in for loop
+                selectedText = self.hand[i].text; //set global selected text
+                selectedPack = self.hand[i].pack; //set global selected pack
+                handCards.forEach(element => { //for each card element
+                    element.classList.remove("selected-card"); //remove selected class from all cards
+                    element.style.backgroundColor = "white"; //reset card background color
+                    element.style.color = "black"; //reset card text color
                 });
                 // Add the selected class to the clicked card
-                this.classList.add("selected-card");
-                hasCardBeenSelected = true;
-                submitButton.disabled = false;
+                this.classList.add("selected-card"); //add selected card class to clicked card
+                hasCardBeenSelected = true; //a card has been selected
+                submitButton.disabled = false; //re-enable submit button
             }
         });
     }
 
-    function getProperName(name){
+    function getProperName(name){ //returns the full formatted name of a pack given an inputted shorthand
         switch(name){
             case "uwu":
                 return "UwU Pack";
@@ -115,110 +112,111 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    submitButton.onclick = () => {
-        if(hasCardBeenSelected) {
-            let payload = {
+    submitButton.onclick = () => { //on submit button click
+        if(hasCardBeenSelected) { //only execute if a card is selected
+            let payload = { //data to be sent to server
                 username: self.name,
                 submission: selectedText,
                 submissionPack: selectedPack,
                 submissionIndex: selectedIndex,
                 id: self.id
             }
-            socket.emit('submit-cards', payload, (response) => {
-                self = response.rawPlayerInfo;
-                populateCardsFromHand(self);
-                submittedText = selectedText;
-                submittedPack = selectedPack;
-                handCards.forEach(element => {
+            socket.emit('submit-cards', payload, (response) => { //send cards to server
+                self = response.rawPlayerInfo; //update self based on server callback
+                populateCardsFromHand(self); //reset cards in hand
+                submittedText = selectedText; //set global submitted text to currently selected text
+                submittedPack = selectedPack; //set global submitted pack to currently selected pack
+                handCards.forEach(element => { //reset all cards and make hand unclickable to prevent more submissions
                     element.classList.remove("selected-card");
                     element.classList.remove("clickable");
                     element.style.backgroundColor = "white"; // Reset background color
                     element.style.color = "black"; // Reset text color
                 });
             });
-            submitButton.disabled = true;
+            submitButton.disabled = true; //disable submit button
+            hasCardBeenSubmitted = true;
         }
     };
 
-    let submittedPublicCardElements = [];
+    let submittedPublicCardElements = []; //hold submitted card HTML elements
     socket.on("pushSubmittedCards", (payload) => {
-        displaySubmittedCards(payload.submissions, payload.showContent)
+        displaySubmittedCards(payload.submissions, payload.showContent) //display all clients' submitted cards
     })
     function displaySubmittedCards(submissions, showContent){
-        submittedPublicCardElements.forEach(card => {
+        submittedPublicCardElements.forEach(card => { //remove all current elements
             card.remove();
         })
-        let firstCard = true;
+        let firstCard = true; //to track which card is the first card
         submissions.forEach ( card => {
-            const submittedCard = document.createElement("div");
-            submittedPublicCardElements.push(submittedCard);
-            const cardText = document.createElement("p");
-            const cardPack = document.createElement("p")
-            submittedCard.classList.add("white-card");
-            cardText.classList.add("white-card-text");
-            cardPack.classList.add("white-card-pack");
-            if(showContent){
-                cardText.textContent = card.text;
-                cardPack.textContent = getProperName(card.pack);
-                cardText.style.display = "p";
-                cardPack.style.display = "p";
-            } else if (firstCard && !self.czar) {
-                firstCard = false;
-                cardText.textContent = submittedText;
-                cardPack.textContent = getProperName(submittedPack);
-                cardText.style.display = "p";
-                cardPack.style.display = "p";
-            } else {
-                cardText.style.display = "none";
-                cardPack.style.display = "none";
+            const submittedCard = document.createElement("div"); //create new card
+            submittedPublicCardElements.push(submittedCard); //add to element list
+            const cardText = document.createElement("p"); //create text object child
+            const cardPack = document.createElement("p"); //create pack object child
+            submittedCard.classList.add("white-card"); //add white card class for CSS formatting
+            cardText.classList.add("white-card-text"); //add white card text class for CSS formatting
+            cardPack.classList.add("white-card-pack"); //add white card pack class for CSS formatting
+            if(showContent){ //if content is to be shown to all
+                cardText.textContent = card.text; //set card text content
+                cardPack.textContent = getProperName(card.pack); //set pack text
+                cardText.style.display = "p"; //make card text visible
+                cardPack.style.display = "p"; //make card pack visible
+            } else if (firstCard && hasCardBeenSubmitted) { //if it's the first card
+                firstCard = false; //disable first card
+                cardText.textContent = submittedText; //display personal submission text on first card
+                cardPack.textContent = getProperName(submittedPack); //display personal submission pack on first card
+                cardText.style.display = "p"; //make card text visible
+                cardPack.style.display = "p"; //make card pack visible
+            } else { //else do not show any text content; display blank card, applies to czar and unsubmitted players
+                cardText.style.display = "none"; //hide card text
+                cardPack.style.display = "none"; //hide card pack
             }
-            submittedCard.appendChild(cardText);
-            submittedCard.appendChild(cardPack);
-            publicContainer.appendChild(submittedCard);
+            submittedCard.appendChild(cardText); //add text as child of card element
+            submittedCard.appendChild(cardPack); //add pack as child of card element
+            publicContainer.appendChild(submittedCard); //add card as child of card container
         });
     }
 
-    let connectedPlayerObjects = [];
+    let connectedPlayerObjects = []; //HTML elements for displaying players
 
-    function updateConnectedPlayers(playerInfo) {
-        connectedPlayerObjects.forEach(item => {
+    function updateConnectedPlayers(playerInfo) { //update list of connected players on client side
+        connectedPlayerObjects.forEach(item => { //remove all existing elements
             item.remove();
         })
-        for (let i = 0; i < playerInfo.length; i++) {
-            let newPlayerObject = document.createElement("div");
-            newPlayerObject.classList.add("player-score-item");
-
-            let newPlayerObjectUsername = document.createElement("h");
-            let newPlayerObjectScore = document.createElement("h");
-            newPlayerObjectUsername.classList.add("player-score-item-username");
-            newPlayerObjectScore.classList.add("player-score-item-score");
-            newPlayerObjectUsername.textContent = playerInfo[i].name;
-            newPlayerObjectScore.textContent = playerInfo[i].score;
-            if(i % 2 === 1)  {
-                newPlayerObject.style.backgroundColor = "#d1d1e0"
+        for (let i = 0; i < playerInfo.length; i++) { //for each current player
+            let newPlayerObject = document.createElement("div"); //create new player/score container object
+            newPlayerObject.classList.add("player-score-item"); //add class for CSS
+            let newPlayerObjectUsername = document.createElement("h"); //create username text
+            let newPlayerObjectScore = document.createElement("h"); //create score text
+            newPlayerObjectUsername.classList.add("player-score-item-username"); //add class for CSS
+            newPlayerObjectScore.classList.add("player-score-item-score"); //add class for CSS
+            newPlayerObjectUsername.textContent = playerInfo[i].name; //set username text content
+            newPlayerObjectScore.textContent = playerInfo[i].score; //set score text content
+            if(i % 2 === 1)  { //change color every other iteration
+                newPlayerObject.style.backgroundColor = "#d1d1e0" //lighter grey
             } else {
-                newPlayerObject.style.backgroundColor = "#a3a3c2"
+                newPlayerObject.style.backgroundColor = "#a3a3c2" //darker gray
             }
 
-            if(playerInfo[i].czar){
-                newPlayerObject.style.backgroundColor = "#6b6b78"
+            if(playerInfo[i].czar){ //if player is czar, set color to a darker gray
+                newPlayerObject.style.backgroundColor = "#6b6b78" //even darker gray
             }
-            newPlayerObject.appendChild(newPlayerObjectUsername);
-            newPlayerObject.appendChild(newPlayerObjectScore);
-            playerContainer.appendChild(newPlayerObject);
-            connectedPlayerObjects.push(newPlayerObject);
+
+            newPlayerObject.appendChild(newPlayerObjectUsername); //add username as child of player object
+            newPlayerObject.appendChild(newPlayerObjectScore); //add score as child of player object
+            playerContainer.appendChild(newPlayerObject); //add player as child of main container object
+            connectedPlayerObjects.push(newPlayerObject); //add player object to list of player elements
         }
     }
 
-    function setCardsClickable(state){
-        handCards.forEach(card => {
-            if(state) {
-                if (!card.classList.contains("clickable")) {
-                    card.classList.add("clickable")
+    function setCardsClickable(state){ //set all cards' clickability
+        handCards.forEach(card => { //for each card
+            if(state) { //if set to true
+                if (!card.classList.contains("clickable")) { //if card does not have clickable class
+                    card.classList.add("clickable") //add clickable class
                 }
             }
             else {
-                card.classList.remove("clickable");
+                card.classList.remove("clickable"); //remove clickable class
             }
         });
     }

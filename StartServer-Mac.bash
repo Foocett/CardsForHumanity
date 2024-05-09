0646470031c164215a32f2c8131ad7e2068a007f
Andrew Fawcett <141191160+Foocett@users.noreply.github.com>
2024-05-09 15:55:35 -0500

    Updated shell files

--- StartServer-Mac.bash
+++ StartServer-Mac.bash
@@ -1,31 +1,3 @@
-#!/bin/bash
-
-# Check for Command Line Tools for Xcode
-if ! xcode-select -p &> /dev/null; then
-  echo "Installing Command Line Tools for Xcode..."
-  xcode-select --install
-  read -p "Press enter once installation is complete" # Wait for user to confirm installation
-fi
-
-# Function to install Node.js using nvm
-install_node() {
-  echo "Installing Node.js..."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Load nvm
-  nvm install node # Install the latest version of node
-  nvm use node
-}
-
-# Check if node is installed
-if ! command -v node &> /dev/null
-then
-    echo "Node.js is not installed."
-    install_node
-else
-    echo "Node.js is already installed."
-fi
-
 # Run main.js file
 node main.js &
 pid=$! # Process ID of the node command
--- StartServer-Windows.bat
+++ StartServer-Windows.bat
@@ -1,15 +1,5 @@
 @echo off
 cls
-
-:: Check for Node.js and install if not present
-where node >nul 2>&1
-if %ERRORLEVEL% NEQ 0 (
-    echo Node.js is not installed. Installing...
-    call :installNode
-) else (
-    echo Node.js is already installed.
-)
-
 :: Run the JavaScript file
 node main.js
 
@@ -24,16 +14,3 @@ for /f "tokens=* USEBACKQ" %%f in (`powershell -Command "(Invoke-WebRequest -uri
     set public_ip=%%f
 )
 echo Your public IP address is: %public_ip%
-
-:: Wait for user input to close the script
-pause
-exit
-
-:: Function to install Node.js using nvm-windows
-:installNode
-    powershell -Command "Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.9/nvm-setup.zip -OutFile nvm-setup.zip"
-    powershell -Command "Expand-Archive -Path nvm-setup.zip -DestinationPath .\nvm-setup"
-    cd nvm-setup
-    start nvm-setup.exe
-    echo Please install Node.js using nvm and restart the script.
-    exit
